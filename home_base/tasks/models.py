9017ae928025a51c6914f8b7a9c435d7c1adccf1
bernoreitsma <reitsmaberno@gmail.com>
2026-05-25 13:16:16 +0200

    make unique constraint on rank deferrable so we can update order

--- home_base/tasks/models.py
+++ home_base/tasks/models.py
@@ -12,9 +12,18 @@ class Task(models.Model):
         FUN = "FUN"
         SMALL = "SMALL"
 
-    rank = models.IntegerField(unique=True)
+    rank = models.IntegerField()
     description = models.CharField(max_length=1000)
     notes = models.CharField(max_length=1000, null=True, blank=True)
     status = models.CharField(max_length=25, choices=TaskStatus, default=TaskStatus.TODO)
     marked = models.BooleanField(default=False)
     category = models.CharField(max_length=25, choices=TaskCategory, null=True, blank=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["rank"],
+                deferrable=models.Deferrable.DEFERRED,
+                name="unique_rank_deferrable"
+            )
+        ]
